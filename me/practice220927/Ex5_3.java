d0f07acee00daf95fd4827da7905ba62632c026b
yunhuiGwak <kwakyh17@naver.com>
2022-10-12 00:00:48 +0900

    test: 220927 practice

--- me/practice220927/Ex5_3.java
+++ me/practice220927/Ex5_3.java
@@ -1,2 +1,14 @@
-package me.practice220927;public class Ex5_3 {
+package me.practice220927;
+
+public class Ex5_3 {
+    public static void main(String[] args) {
+        int[] score = { 79, 88, 91, 33, 100, 55, 95};
+
+        int max = score[0];
+        int min = score[0];
+
+        for (int i=1; i < score.length; i++){       // 0번째 요소는 이미 max,min으로 초기화했으므로 1
+
+        }
+    }
 }
