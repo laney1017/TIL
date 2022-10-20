8c08b8f8f1b4e39c37656c1ac63c12ed2edd58ca
yunhuiGwak <kwakyh17@naver.com>
2022-10-20 23:30:52 +0900

    initial commit

---
+++ me/java/toyproject/src/me/smartsore/project/exception/InputEmptyException.java
@@ -0,0 +1,12 @@
+package me.smartsore.project.exception;
+
+public class InputEmptyException extends RuntimeException {
+
+    public InputEmptyException() {
+        super(Message.ERR_MSG_INVALID_INPUT_EMPTY);
+    }
+
+    public InputEmptyException(String message) {
+        super(message);
+    }
+}
