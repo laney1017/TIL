0182d60285e6a60d9da15f83019ff941f61277d8
yunhuiGwak <kwakyh17@naver.com>
2022-10-24 23:51:27 +0900

    revise code

--- me/jungsuk_basic/ch12/Ex12_1.java
+++ me/jungsuk_basic/ch12/Ex12_1.java
@@ -6,7 +6,7 @@ class Product {}
 class Tv extends Product {}
 class Audio extends Product {}
 
-public class Ex12_1 {
+class Ex12_1 {
     public static void main(String[] args) {
         ArrayList<Product> productList = new ArrayList<Product>();
         ArrayList<Tv>  tvList = new ArrayList<Tv>();
