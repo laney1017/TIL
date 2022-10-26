178ff47027711fcac0ae0868775fca682ab25a4f
yunhuiGwak <kwakyh17@naver.com>
2022-10-26 23:00:09 +0900

    revise code

--- me/java/toyproject/src/me/smartsore/project/customers/Customer.java
+++ me/java/toyproject/src/me/smartsore/project/customers/Customer.java
@@ -16,6 +16,55 @@ public class Customer {
         this.serialNo = String.format("%04d", AUTO_GENERATOR);  // 10진수4자리형식
     }
 
+    public Customer(String name, String userID, int spentTime, int totalPay){
+        ++AUTO_GENERATOR;
+        this.serialNo = String.format("%04d", AUTO_GENERATOR);  // 10진수4자리형식
+        this.name = name;
+        this.userID = userID;
+        this.spentTime = spentTime;
+        this.totalPay = totalPay;
+    }
+
+    public String getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(String serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public int getSpentTime() {
+        return spentTime;
+    }
+
+    public void setSpentTime(int spentTime) {
+        this.spentTime = spentTime;
+    }
+
+    public int getTotalPay() {
+        return totalPay;
+    }
+
+    public void setTotalPay(int totalPay) {
+        this.totalPay = totalPay;
+    }
+
     @Override
     public String toString() {
         return "Customer{" +
--- me/java/toyproject/src/me/smartsore/project/groups/Group.java
+++ me/java/toyproject/src/me/smartsore/project/groups/Group.java
@@ -1,5 +1,46 @@
 package me.smartsore.project.groups;
 
+// 스마트스토어 총 이용시간과 결제금액에 따라 고객을 세 그룹으로 분류
 public class Group {
+    private Group groupType;  // general, vip, vvip
+    private int spentTime;
+    private int totalPay;
 
+    public Group(Group groupType, int spentTime, int totalPay) {
+        this.groupType = groupType'
+        this.spentTime = spentTime;
+        this.totalPay = totalPay;
+    }
+
+    public Group getGroupType() {
+        return groupType;
+    }
+
+    public void setGroupType(Group groupType) {
+        this.groupType = groupType;
+    }
+
+    public int getSpentTime() {
+        return spentTime;
+    }
+
+    public void setSpentTime(int spentTime) {
+        this.spentTime = spentTime;
+    }
+
+    public int getTotalPay() {
+        return totalPay;
+    }
+
+    public void setTotalPay(int totalPay) {
+        this.totalPay = totalPay;
+    }
+
+    @Override
+    public String tostring() {
+        return "Group{" +
+                "groupType = " + groupType +
+                ", spentTime = " + spentTime +
+                ", totalPay = " + totalPay + "}";
+    }
 }
