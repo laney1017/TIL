57884fb9a5630eb5d9b694a7e3a06985a3ae06ed
yunhuiGwak <kwakyh17@naver.com>
2022-11-13 23:37:05 +0900

    first upload

---
+++ me/jungsuk_basic/ch06/Ex6_4.java
@@ -0,0 +1,34 @@
+package me.jungsuk_basic.ch06;
+
+public class Ex6_4 {
+    public static void main(String[] args) {
+        MyMath mm = new MyMath();
+        long result1 = mm.add(5L, 3L);
+        long result2 = mm.subtract(5L, 3L);
+        long result3 = mm.multiply(5L, 3L);
+        double result4 = mm.divide(5L, 3L); // double로 자동 형변환
+
+        System.out.println("add(5L, 3L) =  " + result1);
+        System.out.println("subtract(5L, 3L) = " + result2);
+        System.out.println("multiply(5L, 3L) = " + result3);
+        System.out.println("divide(5L, 3L) = " + result4);
+    }
+}
+
+class MyMath {
+    long add(long a, long b) {
+        long result = a + b;
+        return result;
+        // return a + b; // 위의 두 줄을 이와 같이 한 줄로 간단히 할 수 있다.
+    }
+    long subtract(long a, long b) { return a - b; }
+    long multiply(long a, long b) { return a * b; }
+    double divide(double a, double b) {
+        return a / b;
+    }
+}
+//실행 화면
+//add(5L, 3L) =  8
+//subtract(5L, 3L) = 2
+//multiply(5L, 3L) = 15
+//divide(5L, 3L) = 1.6666666666666667
