fd72c8f05f64a0b25aa5eec45004e29380cf46ce
yunhuiGwak <kwakyh17@naver.com>
2022-12-01 17:20:17 +0900

    [feat] 백준 문제풀이

---
+++ me/baekjoon/Code10950.java
@@ -0,0 +1,23 @@
+package me.baekjoon;
+
+import java.util.Scanner;
+
+public class Code10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a,b;
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
+
+//10950번 A+B -3
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+//각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//각 테스트 케이스마다 A+B를 출력한다.
+
