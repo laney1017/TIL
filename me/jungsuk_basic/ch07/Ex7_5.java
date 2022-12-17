84b65d4f0228bf8bdb82e4c395ea03219e6a14b6
yunhuiGwak <kwakyh17@naver.com>
2022-12-18 00:12:45 +0900

    Initial commit jungsuk_basic code

---
+++ me/jungsuk_basic/ch07/Ex7_5.java
@@ -0,0 +1,11 @@
+package me.jungsuk_basic.ch07; //Ex7_5클래스가 속할 패키지의 선언
+
+public class Ex7_5 {
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
+    }
+}
+//실행결과
+//Hello, World!
+//패키지 선언문은 반드시 소스파일에서 첫 번째 문장이어야 하며 단 한번만 선언할 수 있다.
+//패키지명은 소문자로 하는 것을 원칙
