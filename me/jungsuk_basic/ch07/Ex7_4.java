2e62f12c94029ada81edccac12dc9ad0b3a4c5e6
yunhuiGwak <kwakyh17@naver.com>
2022-12-18 00:08:16 +0900

    Initial commit jungsuk_basic code

---
+++ me/jungsuk_basic/ch07/Ex7_4.java
@@ -0,0 +1,29 @@
+package me.jungsuk_basic.ch07;
+
+public class Ex7_4 {
+    public static void main(String args[]) {
+        Point3D p = new Point3D(1, 2, 3);
+        System.out.println("x=" + p.x + ",y=" + p.y + ",z=" + p.z);
+    }
+}
+class Point {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+class Point3D extends Point {
+    int z;
+
+    Point3D(int x, int y, int z) {
+        super(x,y); // Point(int x, int y)를 호출
+        this.z = z;
+    }
+}
+//실행결과
+//x=1,y=2,z=3
+//super()는 생성자, 조상의 생성자를 호출하는데 사용된다.
+//조상의 멤버는 조상의 생성자를 통해 초기화되도록 작성하는 것이 바람직하다.
+//생성자의 첫 줄에 반드시 생성자를 호출해야 한다. 그렇지 않으면 컴파일러가 생성자의 첫 줄에 super();를 삽입한다.
