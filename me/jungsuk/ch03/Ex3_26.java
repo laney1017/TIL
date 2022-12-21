229e8635b08733e3a0b73b1375bea776cd845eb2
yunhuiGwak <kwakyh17@naver.com>
2022-12-21 17:21:29 +0900

    Initial commit jungsuk code

---
+++ me/jungsuk/ch03/Ex3_26.java
@@ -0,0 +1,20 @@
+package me.jungsuk.ch03;
+
+public class Ex3_26 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+
+        System.out.printf("a=%d, b=%d%n", a, b);
+        System.out.printf("a!=0 || ++b!=0 = %b%n", a != 0 || ++b != 0);
+        System.out.printf("a=%d, b=%d%n", a, b);
+        System.out.printf("a==0 && ++b!=0 = %b%n", a == 0 && ++b != 0);
+        System.out.printf("a=%d, b=%d%n", a, b);
+    }
+}
+//실행결과
+//a=5, b=0
+//a!=0 || ++b!=0 = true
+//a=5, b=0
+//a==0 && ++b!=0 = false
+//a=5, b=0
