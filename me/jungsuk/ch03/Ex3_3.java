a2061557689ce6a8929ff0663caf45db0f4eeba5
yunhuiGwak <kwakyh17@naver.com>
2022-12-21 13:41:50 +0900

    Initial commit jungsuk code

---
+++ me/jungsuk/ch03/Ex3_3.java
@@ -0,0 +1,14 @@
+package me.jungsuk.ch03;
+
+public class Ex3_3 {
+    public static void main(String[] args) {
+        int i=5, j=5;
+        System.out.println(i++);    //println(i++); 은 println()에게 i에 저장된 값 5를 넘겨주고 나서 i값이 증가하기 때문에 5가 출력
+        System.out.println(++j);
+        System.out.println("i = " + i + ", j = " + j);
+    }
+}
+//실행결과
+//5
+//6
+//i = 6, j = 6
