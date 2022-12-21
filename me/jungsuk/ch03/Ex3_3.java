657591ecaff8aa8a3d340ae3e91bb47ca1d296ad
yunhuiGwak <kwakyh17@naver.com>
2022-12-21 13:46:46 +0900

    코드풀이 추가

--- me/jungsuk/ch03/Ex3_3.java
+++ me/jungsuk/ch03/Ex3_3.java
@@ -5,6 +5,11 @@ public class Ex3_3 {
         int i=5, j=5;
         System.out.println(i++);    //println(i++); 은 println()에게 i에 저장된 값 5를 넘겨주고 나서 i값이 증가하기 때문에 5가 출력
         System.out.println(++j);
+        /* System.out.println(i);
+                            i++;
+                            ++j;
+           System.out.println(j); 와 같다.
+         */
         System.out.println("i = " + i + ", j = " + j);
     }
 }
