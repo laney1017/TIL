51360a16431e0b213ee4796b80c164d1a3ccd677
yunhuiGwak <kwakyh17@naver.com>
2022-12-21 15:38:58 +0900

    Initial commit jungsuk code

---
+++ me/jungsuk/ch03/Ex3_19.java
@@ -0,0 +1,14 @@
+package me.jungsuk.ch03;
+
+public class Ex3_19 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 8;
+
+        System.out.printf("%d을 %d로 나누면, %n", x, y);
+        System.out.printf("몫은 %d이고, 나머지는 %d입니다.%n", x/y, x%y);
+    }
+}
+//실행결과
+//10을 8로 나누면,
+//몫은 1이고, 나머지는 2입니다.
