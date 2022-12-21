8724bec15d8d0e38bb742e813a1b52158d6544a3
yunhuiGwak <kwakyh17@naver.com>
2022-12-21 15:01:07 +0900

    Initial commit jungsuk code

---
+++ me/jungsuk/ch03/Ex3_13.java
@@ -0,0 +1,16 @@
+package me.jungsuk.ch03;
+
+public class Ex3_13 {
+    public static void main(String[] args) {
+        char c1 = 'a';
+
+//      char c2 = c1 +1;
+//      수식에 변수가 들어가 있는 경우 -> 컴파일러가 미리 계산을 할 수 없다. 형변환필요
+
+        char c2 = 'a' + 1;
+//      상수 또는 리터럴 간의 연산은 실행과정동안 변하는 값이 아니기 때문에 컴파일러가 계산가능.
+        System.out.println(c2);
+    }
+}
+//실행결과
+//b
