e5c1864c9f4c884949dca9655eff7e535e2c826f
yunhuiGwak <kwakyh17@naver.com>
2022-12-21 17:28:36 +0900

    Initial commit jungsuk code

---
+++ me/jungsuk/ch03/Ex3_27.java
@@ -0,0 +1,29 @@
+package me.jungsuk.ch03;
+
+public class Ex3_27 {
+    public static void main(String[] args) {
+        boolean b = true;
+        char ch = 'C';
+
+        System.out.printf("b=%b%n", b);
+        System.out.printf("!b=%b%n", !b);
+        System.out.printf("!!b=%b%n", !!b);
+        System.out.printf("!!!b=%b%n", !!!b);
+        System.out.println();
+
+        System.out.printf("ch=%c%n", ch);
+        System.out.printf("ch < 'a' || ch > 'z'=%b%n", ch < 'a' || ch > 'z');
+        System.out.printf("!('a' <= ch && ch <= 'z')=%b%n", !('a' <= ch && ch <= 'z'));
+        System.out.printf("  'a' <= ch && ch <= 'z'=%b%n", 'a' <= ch && ch <= 'z');
+    }
+}
+//실행결과
+//b=true
+//!b=false
+//!!b=true
+//!!!b=false
+//
+//ch=C
+//ch < 'a' || ch > 'z'=true
+//!('a' <= ch && ch <= 'z')=true
+//  'a' <= ch && ch <= 'z'=false
