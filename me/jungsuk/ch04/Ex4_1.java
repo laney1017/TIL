28f67ca11adadd151d95184f71c2bffbefc84607
yunhuiGwak <kwakyh17@naver.com>
2022-12-22 17:21:40 +0900

    Redo jungsuk code

--- me/jungsuk/ch04/Ex4_1.java
+++ me/jungsuk/ch04/Ex4_1.java
@@ -3,11 +3,11 @@ package me.jungsuk.ch04;
 public class Ex4_1 {
     public static void main(String[] args) {
         int x = 0;
-        System.out.printf("x=%d 일 때, 참인 것은%n", x);
+        System.out.printf("x=%d 일 떄, 참인 것은%n", x);
 
         if (x==0) System.out.println("x==0");
         if (x!=0) System.out.println("x!=0");
-        if (!(x==0)) System.out.println("!(x==0)");
+        if (!(x==0)) System.out.printf("!(x==0)");
         if (!(x!=0)) System.out.println("!(x!=0)");
 
         x = 1;
@@ -15,12 +15,12 @@ public class Ex4_1 {
 
         if (x==0) System.out.println("x==0");
         if (x!=0) System.out.println("x!=0");
-        if (!(x==0)) System.out.println("!(x==0)");
+        if (!(x==0)) System.out.printf("!(x==0)");
         if (!(x!=0)) System.out.println("!(x!=0)");
     }
 }
 // 실행 결과
-// x=0 일 때, 참인 것은
+//x=0 일 때, 참인 것은
 //x==0
 //!(x!=0)
 //x=1 일 때, 참인 것은
