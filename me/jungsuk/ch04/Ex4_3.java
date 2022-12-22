379b29ee10ab245f6e9019389bcbde17285ad026
yunhuiGwak <kwakyh17@naver.com>
2022-12-22 17:37:38 +0900

    Redo jungsuk code

--- me/jungsuk/ch04/Ex4_3.java
+++ me/jungsuk/ch04/Ex4_3.java
@@ -6,14 +6,14 @@ public class Ex4_3 {
     public static void main(String[] args) {
         System.out.print("숫자를 하나 입력하세요.>");
         Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt(); // 화면을 통해 입력받은 숫자를 input에 저장
+        int input = scanner.nextInt();   // 솨면을 통해 입력받은 숫자를 input에 저장
 
         if (input == 0) {
             System.out.println("입력하신 숫자는 0입니다.");
-        } else { // input!=0인 경우
+        } else {    // input != 0인 경우
             System.out.println("입력하신 숫자는 0이 아닙니다.");
         }
-    } // main의 끝
+    }
 }
 //실행결과1
 //숫자를 하나 입력하세요.>5
