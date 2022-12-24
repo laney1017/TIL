86ea9376f066abd3a4e4cbd0421837d66265cd77
yunhuiGwak <kwakyh17@naver.com>
2022-12-24 23:32:23 +0900

    Initial commit jungsuk code

---
+++ me/jungsuk/ch04/Ex4_12.java
@@ -0,0 +1,20 @@
+package me.jungsuk.ch04;
+
+public class Ex4_12 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++)
+            System.out.println(i);     // i의 값을 출력한다.
+
+        for (int i=1; i<=5; i++)
+            System.out.print(i);       // print() 를 쓰면 가로로 출력된다.
+
+        System.out.println();
+    }
+}
+// 실행결과
+//1
+//2
+//3
+//4
+//5
+//12345
