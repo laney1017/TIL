06b0630e8a91d61aea0f0ede9515ca3b6d214c52
yunhuiGwak <kwakyh17@naver.com>
2022-12-26 09:55:22 +0900

    Initial commit jungsuk code

---
+++ me/jungsuk/ch04/Ex4_23.java
@@ -0,0 +1,17 @@
+package me.jungsuk.ch04;
+
+public class Ex4_23 {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while (i-- != 0) {
+            System.out.println(i + " - I can do it.");
+        }
+    }
+}
+// 실행결과
+//4 - I can do it.
+//3 - I can do it.
+//2 - I can do it.
+//1 - I can do it.
+//0 - I can do it.
