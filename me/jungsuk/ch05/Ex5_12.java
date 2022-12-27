8ced52db196b60132d234363f52e26c19b2733d9
yunhuiGwak <kwakyh17@naver.com>
2022-12-27 14:40:26 +0900

    Initial commit jungsuk code

---
+++ me/jungsuk/ch05/Ex5_12.java
@@ -0,0 +1,26 @@
+package me.jungsuk.ch05;
+
+public class Ex5_12 {
+    public static void main(String[] args) {
+        String[] names = {"Kim", "Park", "Yi"};
+
+        for (int i = 0; i < names.length; i++)
+            System.out.println("names[" + i + "]:" + names[i]);
+
+        String tmp = names[2];    // 배열 names의 3번째 요소를 tmp에 저장
+        System.out.println("tmp:" + tmp);
+        names[0] = "Yu";          // 배열 names의 첫 번째 요소를 "Yu"로 변경
+
+        for(String str : names)    // 향상된 for문
+            System.out.println(str);
+    }
+}
+
+// 실행결과
+//names[0]:Kim
+//names[1]:Park
+//names[2]:Yi
+//tmp:Yi
+//Yu
+//Park
+//Yi
