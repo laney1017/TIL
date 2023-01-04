bd310a60ac24577190017b866b23dc4517b2379d
yunhuiGwak <kwakyh17@naver.com>
2023-01-04 13:39:20 +0900

    Initial commit jungsuk code

---
+++ me/jungsuk/ch07/Ex7_11.java
@@ -0,0 +1,21 @@
+package me.jungsuk.ch07;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class Ex7_11 {
+    public static void main(String[] args) {
+//        System.out.println(Math.random());
+        out.println(random());
+
+//        System.out.println("Math.PI :" + Math.PI);
+        out.println("Math.PI :" + PI);
+    }
+}
+/* static import문을 사용하면
+   static멤버를 호출할 때 클래스 이름을 생략할 수 있다.
+ */
+
+// 실행결과
+//0.6675873486898073
+//Math.PI :3.141592653589793
