664dce60b01c0032fafef0712ef0abc671e226c3
yunhuiGwak <kwakyh17@naver.com>
2023-01-12 09:32:43 +0900

    Initial commit jungsuk code

---
+++ me/jungsuk/ch08/Ex8_10.java
@@ -0,0 +1,12 @@
+package me.jungsuk.ch08;
+
+public class Ex8_10 {
+    public static void main(String[] args) {
+//        throw new Exception();      // Exception을 고의로 발생시킨다.
+    }
+}
+/* Exception클래스와 그 자손들이 발생할 가능성이 있는 문장들에 대해
+   예외처리를 해주지 않으면 컴파일조차 되지 않는다.
+ */
+// 실행결과
+//java: unreported exception java.lang.Exception; must be caught or declared to be thrown
