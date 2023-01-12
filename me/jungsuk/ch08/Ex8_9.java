855fcbd5bfa67f9e2c8f315327320547fd148149
yunhuiGwak <kwakyh17@naver.com>
2023-01-12 09:31:02 +0900

    fix: 생성자에 String을 넣었을 때 설명 추가

--- me/jungsuk/ch08/Ex8_9.java
+++ me/jungsuk/ch08/Ex8_9.java
@@ -13,6 +13,9 @@ public class Ex8_9 {
         System.out.println("프로그램이 정상 종료되었음.");
     }
 }
+/* Exception인스턴스를 생성할 떄, 생성자에 String을 넣어주면, 이 String이 Exception인스턴스에 메시지로 저장된다.
+   이 메시지는 getMessage()를 이용해서 얻을 수 있따.
+ */
 // 실행결과
 //에러 메시지 : 고의로 발생시켰음.
 //java.lang.Exception: 고의로 발생시켰음.
